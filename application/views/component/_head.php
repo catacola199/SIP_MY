3c0549cbc6e1a2c7c1f138832bde385ba89c2a24
MY <m.yulianto17@gmail.com>
2022-10-10 11:31:19 +0700

    delete nga penting

--- application/views/component/_head.php
+++ application/views/component/_head.php
@@ -15,19 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.32/dist/sweetalert2.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-    <link href="<?= base_url('src') ?>/assets/extra-libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="<?= base_url('src') ?>/assets/extra-libs/c3/c3.min.css" rel="stylesheet">
-    <link href="<?= base_url('src') ?>/assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">
-    <link href="<?= base_url('src') ?>/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
-    <!-- Custom CSS -->
     <link href="<?= base_url('src') ?>/dist/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.12.1/b-2.2.3/b-html5-2.2.3/datatables.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
 </head>
--- application/views/component/_jquery.php
+++ application/views/component/_jquery.php
@@ -1,40 +1,26 @@
 <!-- All Jquery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<!-- <script src="<?= base_url('src') ?>/assets/libs/jquery/dist/jquery.min.js"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
 <script src="<?= base_url('src') ?>/assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script> -->
 
-<!-- apps -->
-<!-- apps -->
-<script src="<?= base_url('src') ?>/dist/js/app-style-switcher.js"></script>
 <script src="<?= base_url('src') ?>/dist/js/feather.min.js"></script>
 <script src="<?= base_url('src') ?>/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
 <script src="<?= base_url('src') ?>/dist/js/sidebarmenu.js"></script>
 <!--Custom JavaScript -->
 <script src="<?= base_url('src') ?>/dist/js/custom.js"></script>
-<!--This page JavaScript -->
-<script src="<?= base_url('src') ?>/assets/extra-libs/c3/d3.min.js"></script>
-<script src="<?= base_url('src') ?>/assets/extra-libs/c3/c3.min.js"></script>
-<script src="<?= base_url('src') ?>/assets/libs/chartist/dist/chartist.min.js"></script>
-<!-- <script src="<?= base_url('src') ?>/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script> -->
-<script src="<?= base_url('src') ?>/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>
-<script src="<?= base_url('src') ?>/assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>
 <script src="<?= base_url('src') ?>/dist/js/pages/dashboards/dashboard1.min.js"></script>
 
-<!-- Jquery Part 2 -->
-<!-- slimscrollbar scrollbar JavaScript -->
-<script src="<?= base_url('src') ?>/assets/extra-libs/sparkline/sparkline.js"></script>
-<!--This page plugins -->
-<script src="<?= base_url('src') ?>/assets/extra-libs/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="<?= base_url('src') ?>/dist/js/pages/datatable/datatable-basic.init.js"></script>
+<script src="https://cdn.datatables.net/v/bs5/dt-1.12.1/b-2.2.3/b-html5-2.2.3/datatables.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 
 <!-- Separate -->
 <script>
+    $(document).ready(function () {
+        $('#multi_col_order').DataTable();
+    });
+
     function deleteConfirm(url) {
         Swal.fire({
             icon: 'warning',
