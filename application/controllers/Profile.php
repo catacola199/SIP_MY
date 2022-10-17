fecf182532465125894881a5947b5e2d8d97ebea
catacola199 <fufideira17@gmail.com>
2022-10-17 10:42:28 +0700

    profile

---
+++ application/controllers/Profile.php
@@ -0,0 +1,103 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Profile extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('All_model');
+		$this->load->model('M_User');
+		if ($this->All_model->isNotLogin()) redirect(site_url(''));
+		$this->load->helper('url');
+	}
+
+	public function index()
+	{
+
+		$this->load->view("dashboard/profil_user");
+	}
+	// Get Save User
+	public function save_user()
+	{
+		$options = [
+			'cost' => 10,
+		];
+
+		$data = array(
+			'nama_pengguna'	        => $this->input->post('nama_pengguna'),
+			'instansi_pengguna'     => $this->input->post('instansi_pengguna'),
+			'email_pengguna'	    => $this->input->post('email_pengguna'),
+			'telepon_pengguna'      => $this->input->post('telepon_pengguna'),
+			'id_role'	            => $this->input->post('id_role'),
+			'username_pengguna'	    => $this->input->post('username_pengguna'),
+			'password_pengguna'	    => sha1(sha1($this->input->post('password_pengguna'))),
+			// 'password_pengguna'	    => password_hash($this->input->post('password_pengguna'),PASSWORD_DEFAULT,$options),
+			'foto_pengguna'		    => $this->M_User->_uploadImage()
+		);
+		$this->M_User->simpandatauser($data);
+		$this->session->set_flashdata('notif', 'Data berhasil disimpan');
+		redirect(base_url('users'));
+	}
+
+	// Edit User
+	public function edit_user($id)
+	{
+		// $data["role"] = $this->All_model->getAllRole();
+		$data["user"] = $this->M_User->getID($id);
+		$this->load->view("component/_editPengguna", $data);
+	}
+
+	// Update User
+	public function update_user()
+	{
+		$id = array(
+			'id_pengguna' => $this->input->post('id_pengguna')
+		);
+
+		if (!empty($_FILES['image']['name'])) {
+			$update_foto = $this->M_User->_uploadImage();
+		} else {
+			$update_foto = $this->input->post('foto_profile');
+		}
+
+		$data = array(
+			'nama_pengguna'	        => $this->input->post('nama_pengguna'),
+			'email_pengguna'	    => $this->input->post('email_pengguna'),
+			'telepon_pengguna'      => $this->input->post('telepon_pengguna'),
+			'id_role'	            => $this->input->post('id_role'),
+			'username_pengguna'	    => $this->input->post('username_pengguna'),
+			'password_pengguna'	    => $this->input->post('password_pengguna'),
+			'foto_pengguna'		    => $update_foto
+		);
+		$this->M_User->updatedatauser($data, $id);
+		$this->session->set_flashdata('notif', 'Data berhasil diupdate');
+		redirect(base_url('users'));
+	}
+
+	public function verifuser($id = null)
+	{
+		$idp = array(
+			'id_pengguna' => $id
+		);
+		$nama = $this->db->get_where('pengguna', ["id_pengguna" => $id])->row()->nama_pengguna;
+		$data = array(
+			'terverifikasi' => 1
+		);
+		$this->M_User->verif_user($data, $idp);
+		$this->session->set_flashdata('notif', ucfirst($nama) . ' berhasil terverifikasi');
+		redirect(base_url('users'));
+	}
+
+	// Delete User akun
+	public function deleteuser($id = null)
+	{
+		if (!isset($id)) show_404();
+
+		if ($this->M_User->del_user($id)) {
+			$this->session->set_flashdata('notif', 'Data berhasil dihapus');
+			redirect(base_url('users'));
+		}
+	}
+}
---
+++ application/views/dashboard/profil_user.php
@@ -0,0 +1,185 @@
+<!-- Head -->
+<?php $this->load->view('component/_head') ?>
+<!-- Head -->
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <span class="loader"></span>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
+        <!-- ============================================================== -->
+
+
+        <!-- ****** Top Header -->
+        <?php $this->load->view('component/_header') ?>
+        <!-- ****** Top Header -->
+
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <?php $this->load->view('component/_sidebar') ?>
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+
+        <!-- Page wrapper  -->
+        <div class="page-wrapper">
+            <!-- Bread crumb and right sidebar toggle -->
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-7 align-self-center">
+                        <!-- <h6 class="page-title text-truncate text-dark font-weight-medium mb-1">Welcome Jason!</h6> -->
+                        <div class="d-flex align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb m-0 p-0">
+                                    <li class="breadcrumb-item"><a href="<?= base_url('dashboard') ?>">Dashboard</a>
+                                        <i class="fas fa-angle-double-right"></i> Master Brosur
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Bread crumb and right sidebar toggle -->
+
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <!-- Row -->
+                <div class="row">
+                    <!-- Column -->
+                    <div class="col-lg-4 col-xlg-3 col-md-5">
+                        <div class="card">
+                            <div class="card-body">
+                                <center class="m-t-30"> <img src="<?= base_url('src') ?>/assets/images/users/2.jpg" class="rounded-circle" width="150" />
+                                    <h4 class="card-title m-t-10">Hanna Gover</h4>
+                                    <h6 class="card-subtitle">Accoubts Manager Amix corp</h6>
+                                    <div class="row text-center justify-content-md-center">
+                                        <div class="col-4"><a href="javascript:void(0)" class="link"><i class="icon-people"></i>
+                                                <font class="font-medium">254</font>
+                                            </a></div>
+                                        <div class="col-4"><a href="javascript:void(0)" class="link"><i class="icon-picture"></i>
+                                                <font class="font-medium">54</font>
+                                            </a></div>
+                                    </div>
+                                </center>
+                            </div>
+                            <div>
+                                <hr>
+                            </div>
+                            <div class="card-body"> <small class="text-muted">Email address </small>
+                                <h6>hannagover@gmail.com</h6> <small class="text-muted p-t-30 db">Phone</small>
+                                <h6>+91 654 784 547</h6> <small class="text-muted p-t-30 db">Address</small>
+                                <h6>71 Pilgrim Avenue Chevy Chase, MD 20815</h6>
+                                <div class="map-box">
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d470029.1604841957!2d72.29955005258641!3d23.019996818380896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e848aba5bd449%3A0x4fcedd11614f6516!2sAhmedabad%2C+Gujarat!5e0!3m2!1sen!2sin!4v1493204785508" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                </div> <small class="text-muted p-t-30 db">Social Profile</small>
+                                <br />
+                                <button class="btn btn-circle btn-secondary"><i class="fab fa-facebook-f"></i></button>
+                                <button class="btn btn-circle btn-secondary"><i class="fab fa-twitter"></i></button>
+                                <button class="btn btn-circle btn-secondary"><i class="fab fa-youtube"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-lg-8 col-xlg-9 col-md-7">
+                        <div class="card">
+                            <div class="card-body">
+                                <form class="form-horizontal form-material mx-2">
+                                    <div class="form-group">
+                                        <label class="col-md-12">Full Name</label>
+                                        <div class="col-md-12">
+                                            <input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="example-email" class="col-md-12">Email</label>
+                                        <div class="col-md-12">
+                                            <input type="email" placeholder="johnathan@admin.com" class="form-control form-control-line" name="example-email" id="example-email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Password</label>
+                                        <div class="col-md-12">
+                                            <input type="password" value="password" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Phone No</label>
+                                        <div class="col-md-12">
+                                            <input type="text" placeholder="123 456 7890" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Message</label>
+                                        <div class="col-md-12">
+                                            <textarea rows="5" class="form-control form-control-line"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-12">Select Country</label>
+                                        <div class="col-sm-12">
+                                            <select class="form-select shadow-none form-control-line">
+                                                <option>London</option>
+                                                <option>India</option>
+                                                <option>Usa</option>
+                                                <option>Canada</option>
+                                                <option>Thailand</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-12">
+                                            <button class="btn btn-success text-white">Update Profile</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                </div>
+                <!-- Row -->
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+
+            <!-- footer -->
+            <?php $this->load->view('component/_footer') ?>
+            <!-- End footer -->
+
+        </div>
+        <!-- End Page wrapper  -->
+
+    </div>
+    <!-- End Wrapper -->
+
+    <!-- Jquery -->
+    <?php $this->load->view('component/_jquery') ?>
+    <!-- End JQuery -->
+
+
+
+
+    <script>
+        $(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
