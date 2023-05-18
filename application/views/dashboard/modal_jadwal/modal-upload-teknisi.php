9b19655fb03e1c4ef1ed0df9f4e31cacf8cdb321
catacola199 <fufideira17@gmail.com>
2023-05-18 14:01:34 +0700

    12

--- application/views/dashboard/modal_jadwal/modal-upload-teknisi.php
+++ application/views/dashboard/modal_jadwal/modal-upload-teknisi.php
@@ -1,24 +1,23 @@
 <?php foreach ($jadwal_tek as $data) : ?>
-    <div class="modal fade" id="tunda-<?= $data->id_permohonan ?>" data-bs-backdrop="static" data-bs-keyboard="false" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+    <div class="modal fade" id="uploadtek-<?= $data->id_permohonan ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg">
             <div class="modal-content">
                 <div class="modal-header bg-primary text-white">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Form Tertunda</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Form Upload Dokumen User </h1>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+
                 <div class="modal-body">
-                    <div class="row">
-                        <div class="col">
-                            <form action="<?php echo base_url('Jadwal_Teknisi/update_tertunda') ?>" method="post" enctype="multipart/form-data" class="pl-3 pr-3" autocomplete="off">
-                                <input type="text" hidden name="id_permohonan" id="id_permohonan" value="<?= $data->id_permohonan ?>">
-                                <div class="form-group">
-                                    <div class="form-floating">
-                                        <input type="hidden" name="no_permohonan" id="no_permohonan" value="<?= $data->no_permohonan ?>">
-                                        <input type="text" class="form-control" id="no_permohonan" placeholder="No Permohonan" value="<?= $data->no_permohonan ?>" disabled>
-                                        <label for="No">No Permohonan</label>
-                                    </div>
-                                </div>
-                                <div class="form-group">
+                    <form action="<?php echo base_url('Jadwal_Teknisi/update_uploadDoc') ?>" method="post" enctype="multipart/form-data" class="pl-3 pr-3">
+                        <!-- <input type="text" hidden name="id_permohonan" id="id_permohonan" value="<?= $data->id_permohonan ?>"> -->
+                        <div class="form-group">
+                            <div class="form-floating">
+                                <input type="hidden" name="no_permohonan" id="no_permohonan" value="<?= $data->no_permohonan ?>">
+                                <input type="text" class="form-control" id="no_permohonan" placeholder="No Permohonan" value="<?= $data->no_permohonan ?>" disabled>
+                                <label for="No">No Permohonan</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
                             <label for="upload">Upload BAP</label>
                             <div class="table-responsive" name="upload">
                                 <table class="table table-borderless">
@@ -48,22 +47,13 @@
                                 </table>
                             </div>
                         </div>
-                                <div class="form-group">
-                                    <div class="form-floating">
-                                        <textarea class="form-control" placeholder="Tulis disini" id="ket_tunda" name="ket_tunda" autocomplete="off"  required></textarea>
-                                        <label for="ket_tunda">Keterangan</label>
-                                    </div>
-                                </div>
 
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal"> <i class="fa fa-window-close"></i> Batal</button>
-                                    <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Simpan</button>
-                                </div>
-                            </form>
-                        </div>
-
-                    </div>
                 </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal"> <i class="fa fa-window-close"></i> Batal</button>
+                    <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Simpan</button>
+                </div>
+                </form>
             </div>
         </div>
     </div>
